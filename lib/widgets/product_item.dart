0574795ee4528c6ff45b16875002e52d0d41c617
subhadeepdash <dash.subhadeep@automationedge.com>
2020-09-01 20:17:33 +0530

    Providers

---
+++ lib/widgets/product_item.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../screens/product_detail_screen.dart';
+import '../providers/product.dart';
+
+class ProductItem extends StatelessWidget {
+  // final String id;
+  // final String title;
+  // final String imageUrl;
+
+  // ProductItem(this.id, this.title, this.imageUrl);
+  @override
+  Widget build(BuildContext context) {
+    final product = Provider.of<Product>(context);
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: GridTile(
+        child: GestureDetector(
+          child: Image.network(
+            product.imageUrl,
+            fit: BoxFit.cover,
+          ),
+          onTap: () {
+            Navigator.of(context).pushNamed(ProductDetailScreen.routeName,
+                arguments: product.id);
+          },
+        ),
+        footer: GridTileBar(
+          backgroundColor: Colors.black87,
+          leading: IconButton(
+            icon: Icon(
+              product.isFavorite ? Icons.favorite : Icons.favorite_border,
+            ),
+            color: Theme.of(context).accentColor,
+            onPressed: product.toggleFavoriteStatus,
+          ),
+          title: Text(
+            product.title,
+            textAlign: TextAlign.center,
+          ),
+          trailing: IconButton(
+            icon: Icon(Icons.shopping_cart),
+            color: Theme.of(context).accentColor,
+            onPressed: () {},
+          ),
+        ),
+      ),
+    );
+  }
+}
